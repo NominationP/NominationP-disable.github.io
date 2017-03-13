eaa8f7e52377ae2b8fbb30a75177a80a32b398ae
zhang ze <605166577@qq.com>
2017-03-13 15:51:29 +0800

    up

--- coder_process/friends/process.php
+++ coder_process/friends/process.php
@@ -6,13 +6,13 @@ class Cut_pr{
     public $file_root;
     public $Season , $Episode , $title , $role , $part , $words_num;
     public $db,$db_friend;
+    public $sea_epi,$file_title; //rewrite
 
-    function __construct($file_root="/var/www/html/NominationP.github.io/_posts/blog/2017-02-22-friends-0120.md"){
-        $this->file_root = $file_root;
+
+    function __construct(){
 
         $this->db = new Mysql;
         $this->db_friend = $this->initial("localhost","root","#mJl&dcs.6(O","Friends");
-
     }
     function initial($db_port,$db_user,$db_password,$db_name){
 
@@ -26,11 +26,122 @@ class Cut_pr{
 
     function run(){
 
+        // $this->from_file_to_datebase();
+        $this->rewrite('01-20');
+    }
+
+
+    public function rewrite($sea_epi){
+
+        $this->sea_epi=$sea_epi;
+        $this->re_pre();
+        $this->re_spe();
+    }
+
+    public function re_spe(){
+
+        /*today_max*/
+        $max=0;
+        $tag;
+        $last_name = "";
+        $last_num = 0;
+        $count = 0;
+        $sql = "select * from detail where season=$this->Season and episode=$this->Episode";
+        $re = $this->db_friend->select($sql);
+
+        foreach ($re as $key => $value) {
+            $count++;
+            $name = $value['name'];
+            $words_num = $value['words_num'];
+            $id = $value['id'];
+
+            if($name == $last_name || $count==1){
+                $last_num += $words_num;
+                $last_name = $name;
+                if($last_num > $max) { $max = $last_num; $tag = $id; }
+            }else{
+
+                $last_num = $words_num;
+                $last_name = $name;
+            }
+        }
+
+        print_r($max."\n".$tag);die();
+
+        $spe_arr=array(
+            ">Role : $this->role",
+            "Part : $this->part",
+            "Words : $this->words_num",
+            "",
+            "`TODAY_MAX` :",
+            );
+
+        foreach ($spe_arr as $key => $value) {
+            $this->write_file($this->file_title,$value,false);
+        }
+    }
+
+    public function re_pre(){
+
+        /*get Season and Episode*/
+        $sea_epi = explode("-", $this->sea_epi);
+        $this->Season = $sea_epi[0];
+        $this->Episode = $sea_epi[1];
+
+        /*get title role part words_num*/
+        $sql = "select * from list where season=$this->Season and episode=$this->Episode";
+        $re = $this->db_friend->select($sql);
+        $this->title = $re[0]['title'];
+        $this->role = $re[0]['role'];
+        $this->part = $re[0]['part_num'];
+        $this->words_num = $re[0]['words_num'];
+
+        /*file_title*/
+        date_default_timezone_set('Asia/Shanghai');
+        $today = date("Y-m-d");
+        $this->file_title = $today."-friends-$this->Season$this->Episode-up.md";
+
+        /*get/set pre of a file*/
+        date_default_timezone_set('Asia/Shanghai');
+        $today = date("Y-m-d H:i:s");
+
+        $pre_arr = array("---","layout: article",
+            "title: Friends $this->Season-$this->Episode $this->title UP",
+            "date:   $today",
+            "modified: $today",
+            "category: blog","tags:","- friends","- english","image:","#  feature:","    teaser: /background/dress.jpg","---","","");
+
+        foreach ($pre_arr as $key => $value) {
+            // $this->write_file($this->file_title,$value);
+        }
+
+
+        // file_put_contents($root_path.$file_path, $string.PHP_EOL , FILE_APPEND | LOCK_EX."\n");
+
+    }
+
+    public function write_file($file_title,$string){
+        $root = "../../_posts/blog/";
+        file_put_contents($root.$file_title, $string.PHP_EOL , FILE_APPEND | LOCK_EX."\n");
+
+    }
+
+
+
+
+
+    /*______________________________________________________________________save_to_mysql_from_file____*/
+
+    // 2017-02-22-friends-0120.md
+    // 2017-02-22-friends-0121.md
+    public function from_file_to_datebase($file_root="../../_posts/blog/2017-02-22-friends-0121.md"){
+
+        $this->file_root = $file_root;
+        print_r("update $file_root ...\n");
         $this->get_Sea_Epi_til_rol();
         $this->collect();
         $this->set_db_Sea_Epi_til_rol();
-
-        // $this->trasn_big();
+        print_r("done\n");
     }
 
     /**
@@ -84,7 +195,7 @@ class Cut_pr{
             'season' => $this->Season,
             'episode' => $this->Episode,
             'title' => $this->title,
-            'role' => $this->role,
+            'role' => $this->ex_change($this->role),
             'part_num' => $this->part,
             'words_num' => $this->words_num,
             );
@@ -156,7 +267,8 @@ class Cut_pr{
 
                             for ($i=1; $i < sizeof($line_arr); $i++) {
                                 /*check if has chinese*/
-                                if(!preg_match('/[A-Za-z0-9.?,!]/', $line_arr[$i])){
+                                // if(!preg_match('/[A-Za-z0-9.?,!]/', $line_arr[$i])){
+                                if(preg_match("/\p{Han}+/u", $line_arr[$i])){
                                     $is_chinese = 1;
                                 }else{
                                     // print_r($line_arr[$i]."(((\n");
@@ -177,12 +289,17 @@ class Cut_pr{
                                 'words'         =>  $whole_line,
                                 'words_num'     =>  $whole_line_count,
                                 'is_chinese'    =>  $is_chinese,
+                                'part'          =>  $part,
                                 'season'        =>  $this->Season,
-                                'episode'        =>  $this->Episode,
+                                'episode'       =>  $this->Episode,
                                 );
                             $condition = array(
                                 'line' => $count,
+                                'season' => $this->Season,
+                                'episode' => $this->Episode,
                                 );
+
+
                             $this->db_friend->smart_up('detail',$arr,$condition);
 
 
@@ -198,7 +315,7 @@ class Cut_pr{
                                 foreach ($line_arr as $key => $value) {
 
                                     /*check if has chinese*/
-                                    if(!preg_match('/[A-Za-z0-9.?,!]/', $value)){
+                                    if(preg_match("/\p{Han}+/u", $value)){
                                         $is_chinese = 1;
                                     }else{
                                         $whole_line_count++;
@@ -217,11 +334,14 @@ class Cut_pr{
                                     'words'         =>  $whole_line,
                                     'words_num'     =>  $whole_line_count,
                                     'is_chinese'    =>  $is_chinese,
+                                    'part'          =>  $part,
                                     'season'        =>  $this->Season,
                                     'episode'        =>  $this->Episode,
                                     );
                                 $condition = array(
                                     'line' => $count,
+                                    'season' => $this->Season,
+                                    'episode' => $this->Episode,
                                     );
 
                                 $this->db_friend->smart_up('detail',$arr,$condition);
